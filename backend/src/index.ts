e265230a2edb06cbc42b8193f90a1d47732808d5
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-06-03 19:08:22 -0300

    add cors in project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import Routes from './routes/routes'
 import path from 'path'
+import cors from 'cors'
 
 const app = express()
 
 class EcoletaIndex {
   Config() {
+    app.use(cors())
     app.use(express.json())
     app.use(Routes)
     app.use('/images', express.static(path.resolve(__dirname, '..', 'assets')))
