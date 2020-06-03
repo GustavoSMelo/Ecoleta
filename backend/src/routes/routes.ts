a113581e100d83a480c8c67e843b3d50d79ff5a8
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-06-03 19:08:07 -0300

    new routes in project

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,11 +1,15 @@
 import express, { Request, Response } from 'express'
 import itemsController from '../controllers/itemController'
+import EcoPointsController from '../controllers/ecoPointsController'
+
 const routes = express.Router()
 
-routes.get('/', (req: Request, res: Response) => {
-  return res.json('hello world')
-})
-
+// ecopoints routes
+routes.post('/ecopoints', EcoPointsController.store)
+routes.get('/ecopoints', EcoPointsController.index)
+routes.get('/ecopoint', EcoPointsController.show)
+routes.get('/ecopoints/filter', EcoPointsController.index_show)
+// items routes
 routes.get('/items', itemsController.index)
 
 export default routes
