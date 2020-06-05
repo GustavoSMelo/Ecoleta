1e04cd17f657c28a435faf4ba286ca643980ad1c
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-06-05 20:45:21 -0300

    style of button

--- frontend/src/components/header/style.ts
+++ frontend/src/components/header/style.ts
@@ -1,11 +1,19 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Container = styled.header`
     width: 100%;
     height: 100px;
     padding: 30px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
 
     figure > img {
         width: 100px;
     }
-`
+
+    h1{
+        color: #070f47;
+        font-size: 16pt;
+    }
+`;
