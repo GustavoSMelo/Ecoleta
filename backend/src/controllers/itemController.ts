fb90f4063d460492763ad6fa895cbf8be0b306d6
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-06-05 20:43:31 -0300

    return id

--- backend/src/controllers/itemController.ts
+++ backend/src/controllers/itemController.ts
@@ -1,31 +1,32 @@
-import 'reflect-metadata'
-import { getConnection, createConnection, Connection } from 'typeorm'
-import { Request, Response } from 'express'
-import ConnectionConfig from '../config/connection'
+import "reflect-metadata";
+import { getConnection, createConnection, Connection } from "typeorm";
+import { Request, Response } from "express";
+import ConnectionConfig from "../config/connection";
 
 class ItemController {
   async index(req: Request, res: Response) {
     try {
-      const connection = await createConnection(ConnectionConfig)
-      const itemRepository = connection.getRepository('items')
+      const connection = await createConnection(ConnectionConfig);
+      const itemRepository = connection.getRepository("items");
 
-      const allItems = await itemRepository.find()
+      const allItems = await itemRepository.find();
 
       const serialized = allItems.map((item) => {
         return {
+          id: item.id,
           title: item.title,
-          image_url: `http://localhost:3333/images/${item.image}`
-        }
-      })
+          image_url: `http://localhost:3333/images/${item.image}`,
+        };
+      });
 
-      res.json(serialized)
+      res.json(serialized);
 
-      return connection.close()
+      return connection.close();
     } catch (err) {
-      res.send('connection lost')
-      console.log(err)
+      res.send("connection lost");
+      console.log(err);
     }
   }
 }
 
-export default new ItemController()
+export default new ItemController();
