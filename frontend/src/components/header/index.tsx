502fe013a7f1234253507b6fadfba6ed904fa161
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-06-05 20:45:07 -0300

    add button in header

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -1,15 +1,29 @@
-import React from 'react'
-import { Container } from './style'
-import Logo from '../../assets/logo.svg'
+import React from "react";
+import { Container } from "./style";
+import Logo from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
 
-const Header = () => {
+const Header = (props: any) => {
+  if (props.returnback) {
     return (
-        <Container>
-            <figure>
-                <img src={Logo} alt="logo-ecoleta" />
-            </figure>
-        </Container>
-    )
-}
+      <Container>
+        <figure>
+          <img src={Logo} alt="logo-ecoleta" />
+        </figure>
+        <Link to="/">
+          <h1><FaArrowLeft color="#34cb79" /> Voltar para home</h1>
+        </Link>
+      </Container>
+    );
+  }
+  return (
+    <Container>
+      <figure>
+        <img src={Logo} alt="logo-ecoleta" />
+      </figure>
+    </Container>
+  );
+};
 
-export default Header
+export default Header;
