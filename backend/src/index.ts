b5334c5de8450d9eacd71d4b16959376bed9dedd
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-06-07 18:37:41 -0300

    add new folder to static files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,8 @@ class EcoletaIndex {
     app.use(cors())
     app.use(express.json())
     app.use(Routes)
-    app.use('/images', express.static(path.resolve(__dirname, '..', 'uploads')))
+    app.use('/photos', express.static(path.resolve(__dirname, '..', 'uploads')))
+    app.use('/images', express.static(path.resolve(__dirname, '..', 'assets')))
   }
 
   Server(server: number = 3333) {
