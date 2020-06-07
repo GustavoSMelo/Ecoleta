8d2f8584bd4b7b9b5431a79d893d78e5db4d6665
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-06-07 16:58:26 -0300

    add multer uploads in route

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,11 +1,14 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 import itemsController from '../controllers/itemController'
 import EcoPointsController from '../controllers/ecoPointsController'
+import multer from 'multer'
+import multerConfig from '../config/multer'
 
 const routes = express.Router()
+const uploads = multer(multerConfig)
 
 // ecopoints routes
-routes.post('/ecopoints', EcoPointsController.store)
+routes.post('/ecopoints', uploads.single('image'), EcoPointsController.store)
 routes.get('/ecopoints', EcoPointsController.index)
 routes.get('/ecopoint', EcoPointsController.show)
 routes.get('/ecopoints/filter', EcoPointsController.index_show)
