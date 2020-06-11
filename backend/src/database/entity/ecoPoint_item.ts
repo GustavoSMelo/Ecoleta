11dfe6ae7a0d58d420e6471f5672fc061e308780
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-06-10 22:39:55 -0300

    update

--- backend/src/database/entity/ecoPoint_item.ts
+++ backend/src/database/entity/ecoPoint_item.ts
@@ -1,7 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
 
 @Entity()
-/* eslint-disable camelcase */
+
 export class ecopoint_item {
   @PrimaryGeneratedColumn()
   id: number
